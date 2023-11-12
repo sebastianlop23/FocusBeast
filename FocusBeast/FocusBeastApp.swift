55696cc3e7b5bb6bdd1d967c9a214efe1edc2b0e
Sebastian Lopez <sdl2@njit.edu>
2023-11-12 16:15:21 -0500

    Initial Commit

---
+++ FocusBeast/FocusBeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBeastApp.swift
+//  FocusBeast
+//
+//  Created by Sebastian Lopez on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusBeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
